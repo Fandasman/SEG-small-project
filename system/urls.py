4264327661a486ad7405f726346226a3c97d373d
Roger20069769 <81314310+Roger20069769@users.noreply.github.com>
2021-12-01 23:24:15 +0000

    Update urls.py

--- system/urls.py
+++ system/urls.py
@@ -20,6 +20,6 @@ from clubs import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name = 'home'),
-    path('log_in', views.log_in, name = 'log_in'),
+    path('log_in/', views.log_in, name = 'log_in'),
 ]
 
