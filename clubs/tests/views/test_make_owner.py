4c5642c2866c56c3f964a83c44ef9adb30efba6f
“Arham <arham.azhary@kcl.ac.uk>
2021-12-13 15:21:20 +0000

    tests added

--- clubs/tests/views/test_make_owner.py
+++ clubs/tests/views/test_make_owner.py
@@ -1,8 +1,9 @@
 from django.test import TestCase
-from django.urls import reverse
 from django.contrib.auth.hashers import check_password
 from clubs.models import User, Club
 from clubs.tests.helpers import LogInTester
+from django.urls import reverse
+from clubs.tests.helpers import reverse_with_next
 
 class MakeOwnerTest(TestCase):
 
@@ -18,8 +19,14 @@ class MakeOwnerTest(TestCase):
         self.club = Club.objects.get(name = 'ChessHub')
         self.url = reverse('make_owner', kwargs={'club_id': self.club.id})
 
+
     def test_password_url(self):
         self.assertEqual(self.url, f'/pass_ownership/{self.club.id}/')
+
+    def test_get_password_redirects_when_not_logged_in(self):
+        redirect_url = reverse_with_next('log_in', self.url)
+        response = self.client.get(self.url)
+        self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
 
 
 
